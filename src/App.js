7dcb68611b92a3c7e94a74b30dd16aded96af2ba
lionroy <lionroy@gmail.com>
2018-07-17 01:56:47 +0300

    styling...

--- src/App.js
+++ src/App.js
@@ -1,35 +1,77 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-import styled from 'styled-components';
+import styled, { keyframes, ThemeProvider } from "styled-components";
 
+const Outer = styled.div`
+  text-align: center;
+`;
 
+const spin = keyframes`
+from { transform: rotate(0deg); }
+to { transform: rotate(360deg); }
+`;
+
+const Logo = styled.img`
+  animation: ${spin} infinite 20s linear;
+  height: 80px;
+`;
+
+const Header = styled.div`
+  background-color: gold;
+  height: 150px;
+  padding: 20px;
+  padding-bottom: 20px;
+  color: royalblue;
+
+  > h2 {
+    ${props => props.theme.upstyle && "text-transform: uppercase"};
+    color: purple;
+  }
+`;
+const Title = styled.div`
+  font-size: 1.5em;
+`;
+const Intro = styled.p`
+  font-size: large;
+`;
 const Button = styled.button`
-display: inline-block;
-  border-radius: 3px;
-  padding: 0.5rem 0;
-  margin: 0.5rem 1rem;
-  width: 11rem;
-  background: transparent;
-  color: green;
-  border: white;
-  border-bottom: 2px solid green;
+display: inline-block
+  border-radius: 3px
+  padding: 0.5rem 0
+  margin: 0.5rem 1rem
+  width: 11rem
+  background: transparent
+  color: green
+  border: white
+  border-bottom: 2px solid green
   `;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started <Button label="OK!">OK!</Button>
-          edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Outer>
+        <ThemeProvider
+          theme={{
+            upstyle: true
+          }}
+          >
+          <Header>
+            <Logo src={logo} alt="logo" />
+            <Title>
+              <h2>Get Ready To React</h2>
+            </Title>
+          </Header>
+        </ThemeProvider>
+        <Intro>
+          To get started say{" "}
+          <Button primary label="OK!">
+            OK!
+          </Button>
+          edit.
+        </Intro>
+      </Outer>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import { injectGlobal } from 'styled-components'
+
+injectGlobal`
+    body {
+        margin: 0;
+        padding: 0;
+        font-family: Helvelica;
+  }
+`
+
+ReactDOM.render(<App />, document.getElementById('root'))
+registerServiceWorker()
