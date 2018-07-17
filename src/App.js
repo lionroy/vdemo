a274af9d51752f25c9b7e0db5d276c17502d331c
lionroy <lionroy@gmail.com>
2018-07-17 20:38:18 +0300

    layouting..

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 
-import styled, { keyframes } from "styled-components";
+import styled, { css, keyframes } from "styled-components";
+
+// fa- fonts lib
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faEnvelope, faKey, faHome } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; // returns in ea page
+
+library.add(faEnvelope, faKey, faHome); //fa envelope
 
 const Outer = styled.div`
   text-align: center;
@@ -19,10 +26,10 @@ const Logo = styled.img`
 
 const Header = styled.div`
   background-color: gold;
-  height: 150px;
-  padding: 20px;
-  padding-bottom: 20px;
-  color: royalblue;
+  height: 4erm;
+  padding: 1erm;
+  padding-bottom: 1em;
+  color: purple;
 
   > h2 {
     ${props => props.theme.uppercase && "text-transform: uppercase"};
@@ -32,8 +39,35 @@ const Header = styled.div`
 const Title = styled.div`
   font-size: 1.5em;
 `;
+const Main = styled.div`
+  font-size: 1.2em;
+  border-radius: 9px
+  padding: 0.5rem 0
+  margin: 0.5rem 1rem
+  width: 75%
+  background: lightblue
+  color: darkblu
+  border: white
+  border-top: 2px solid black
+`;
+const Capsule = styled.div`
+  font-size: 1.2em;
+  border-radius: 9px
+  padding: 0.5rem 0
+  margin: 0.5rem 1rem
+  align: center
+  width: 75%
+  background: #CCC
+  color: darkblue
+  border: white
+  border-top: 2px solid lightgrey
+  border-bottom: 2px solid grey
+`;
 const Intro = styled.p`
   font-size: large;
+`;
+const Label = styled.p`
+  font-size: normal;
 `;
 const Button = styled.button`
 display: inline-block
@@ -41,11 +75,38 @@ display: inline-block
   padding: 0.5rem 0
   margin: 0.5rem 1rem
   width: 11rem
-  background: transparent
-  color: green
-  border: white
-  border-bottom: 2px solid green
+  background: #EDD2EF
+  color: purple
+  border: 2px solid cornflowerblue  
+  
+  ${props =>
+    props.primary &&
+    css`
+  background: #EFE8D2
+  color: purple
+  border: 
+     2px solid gold  
+`}
   `;
+
+const EqualDivider = styled.div`
+  display: flex;
+  margin: 0.5rem;
+  padding: 1rem;
+  background: papayawhip;
+  ${props => props.vertical && "flex-direction: column;"} > * {
+    flex: 1;
+
+    &:not(:first-child) {
+      ${props => (props.vertical ? "margin-top" : "margin-left")}: 1rem;
+    }
+  }
+`;
+
+const Child = styled.div`
+  padding: 0.25rem 0.5rem;
+  background: palevioletred;
+`;
 
 class App extends Component {
   render() {
@@ -58,12 +119,32 @@ class App extends Component {
           </Title>
         </Header>
         <Intro>
-          To get started say{" "}
-          <Button primary label="OK!">
-            OK!
-          </Button>
-          edit.
+          <FontAwesomeIcon icon="home" /> Contact Us This is a simple React
+          Page. Bottom UP. Here goes. Stay Tuned
         </Intro>
+        <Capsule>
+          <Button primary label="Login">
+            <Label>
+              <FontAwesomeIcon icon="key" /> Login
+            </Label>
+          </Button>
+          <Button label="Contact">
+            <Label>
+              <FontAwesomeIcon icon="envelope" /> Contact Us
+            </Label>
+          </Button>
+        </Capsule>
+          <EqualDivider>
+            <Child>First</Child>
+            <Child>Second</Child>
+            <Child>Third</Child>
+          </EqualDivider>
+          <EqualDivider vertical>
+            <Child>First</Child>
+            <Child>Second</Child>
+            <Child>Third</Child>
+          </EqualDivider>
+        <Main />
       </Outer>
     );
   }
